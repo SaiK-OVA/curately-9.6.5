68797e2738d56e9c026e22ae7284a46587ea9c6e
Sai Krishna <saik@ovahq.com>
2025-04-14 12:28:21 +0530

    Update feature X

--- src/components/Dashboard/Home/Home.tsx
+++ src/components/Dashboard/Home/Home.tsx
@@ -677,6 +677,7 @@ const Home = () => {
     return (
         <div className='home_main'>
              <h1>Testing</h1>
+             <h2>Testing from vs code</h2>
             <Card className='pt-1 card_main'>
                 <Grid
                     container
